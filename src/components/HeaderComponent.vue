5f777edf27683d669bf297cb55c36cf3836f0b89
Lionel-Feuga <feuga.lionel1@gmail.com>
2024-11-30 07:42:27 +0100

    HeaderComponent commencé

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref } from "vue";
+
+const menuItems = ref([
+  "Accueil",
+  "À propos",
+  "Compétences",
+  "Portfolio",
+  "Contactez-moi",
+]);
+
+const navigate = (item) => {
+  console.log(`Navigation vers : ${item}`);
+};
+</script>
+
+<template>
+  <v-row align="center" justify="space-between" class="header">
+    <v-col cols="4">
+      <img
+        src="../assets/lfLogo.png"
+        class="logo mt-5 ms-5"
+        alt="Logo Lionel Feuga"
+      />
+    </v-col>
+    <v-col cols="4" class="d-flex ga-5">
+      <v-menu offset-y>
+        <template #activator="{ props }">
+          <v-btn v-bind="props" color="primary" dark>
+            Menu
+          </v-btn>
+        </template>
+
+        <v-list class="d-flex ga-5">
+          <v-list-item v-for="(item, index) in menuItems" :key="index" @click="navigate(item)">
+            <v-list-item-title>{{ item }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-col>
+  </v-row>
+</template>
+
+<style scoped>
+.logo {
+  height: 6.25rem;
+  border-radius: 999px;
+}
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HeaderComponent from '@/components/HeaderComponent.vue';
+
+//
+</script>
+
+<template>
+  <HeaderComponent />
+
+  <img src="../assets/codingMan.png" class="ms-15" style="border-radius: 999px; height: 400px;" alt="la">
+
+</template>
+
+<style scoped>
+
+</style>
