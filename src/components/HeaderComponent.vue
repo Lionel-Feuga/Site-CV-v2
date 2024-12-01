ba28da70a02f35cc09f8bc94d3de4e2a7d4891ed
Lionel <feuga.lionel1@gmail.com>
2024-12-01 15:48:55 +0100

    header: burger-menu/list-veil _ home: base display

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,21 +1,162 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted, nextTick } from "vue";
+import { gsap } from "gsap";
 
-const menuItems = ref([
-  "Accueil",
-  "À propos",
-  "Compétences",
-  "Portfolio",
-  "Contactez-moi",
-]);
+const isOpen = ref(false);
+let spans = [];
+let dropdownMenu = null;
+let dropdownItems = [];
 
-const navigate = (item) => {
-  console.log(`Navigation vers : ${item}`);
+const toggleMenu = async () => {
+  if (!isOpen.value) {
+    isOpen.value = true;
+    await nextTick();
+    dropdownMenu = document.querySelector(".dropdown-menu");
+    dropdownItems = document.querySelectorAll(".dropdown-item");
+    openAnimation();
+  } else {
+    closeAnimation();
+  }
 };
+
+const openBurgerAnimation = () => {
+  gsap.to(spans[0], {
+    left: "5px",
+    top: "7px",
+    rotation: 45,
+    duration: 0.1,
+    backgroundColor: "white",
+  });
+  gsap.to(spans[1], {
+    left: "calc(50% - 5px)",
+    top: "7px",
+    rotation: -45,
+    duration: 0.1,
+    backgroundColor: "white",
+  });
+  gsap.to(spans[2], {
+    left: "-50%",
+    opacity: 0,
+    duration: 0.1,
+    backgroundColor: "white",
+  });
+  gsap.to(spans[3], {
+    left: "100%",
+    opacity: 0,
+    duration: 0.1,
+    backgroundColor: "white",
+  });
+  gsap.to(spans[4], {
+    left: "5px",
+    top: "29px",
+    rotation: -45,
+    duration: 0.1,
+    backgroundColor: "white",
+  });
+  gsap.to(spans[5], {
+    left: "calc(50% - 5px)",
+    top: "29px",
+    rotation: 45,
+    duration: 0.1,
+    backgroundColor: "white",
+  });
+};
+
+const closeBurgerAnimation = () => {
+  gsap.to(spans[0], {
+    left: "0px",
+    top: "0px",
+    rotation: 0,
+    duration: 0.1,
+    backgroundColor: "black",
+  });
+  gsap.to(spans[1], {
+    left: "50%",
+    top: "0px",
+    rotation: 0,
+    duration: 0.1,
+    backgroundColor: "black",
+  });
+  gsap.to(spans[2], {
+    left: "0px",
+    opacity: 1,
+    duration: 0.1,
+    backgroundColor: "black",
+  });
+  gsap.to(spans[3], {
+    left: "50%",
+    opacity: 1,
+    duration: 0.1,
+    backgroundColor: "black",
+  });
+  gsap.to(spans[4], {
+    left: "0px",
+    top: "36px",
+    rotation: 0,
+    duration: 0.1,
+    backgroundColor: "black",
+  });
+  gsap.to(spans[5], {
+    left: "50%",
+    top: "36px",
+    rotation: 0,
+    duration: 0.1,
+    backgroundColor: "black",
+  });
+};
+
+const openAnimation = () => {
+  openBurgerAnimation();
+
+  gsap.fromTo(
+    dropdownMenu,
+    { y: "-100%", opacity: 0 },
+    { y: "0%", opacity: 1, duration: 0.3, ease: "power2.out" }
+  );
+
+  gsap.fromTo(
+    dropdownItems,
+    { y: 50, opacity: 0 },
+    {
+      y: 0,
+      opacity: 1,
+      duration: 0.2,
+      stagger: 0.1,
+      ease: "power2.out",
+      delay: 0.3,
+    }
+  );
+};
+
+const closeAnimation = () => {
+  gsap.to(dropdownItems, {
+    y: "-100%",
+
+    duration: 0.3,
+    ease: "power2.in",
+  });
+
+  gsap.to(dropdownMenu, {
+    y: "-100%",
+    opacity: 0,
+    duration: 0.3,
+    ease: "power2.in",
+
+    onComplete: () => {
+      isOpen.value = false;
+    },
+  });
+
+  closeBurgerAnimation();
+};
+
+onMounted(() => {
+  spans = document.querySelectorAll(".burger span");
+});
 </script>
 
 <template>
-  <v-row align="center" justify="space-between" class="header">
+  <v-row align="center" justify="space-between" class="header d-flex">
     <v-col cols="4">
       <img
         src="../assets/lfLogo.png"
@@ -23,27 +164,127 @@ const navigate = (item) => {
         alt="Logo Lionel Feuga"
       />
     </v-col>
-    <v-col cols="4" class="d-flex ga-5">
-      <v-menu offset-y>
-        <template #activator="{ props }">
-          <v-btn v-bind="props" color="primary" dark>
-            Menu
-          </v-btn>
-        </template>
 
-        <v-list class="d-flex ga-5">
-          <v-list-item v-for="(item, index) in menuItems" :key="index" @click="navigate(item)">
-            <v-list-item-title>{{ item }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
+    <v-col cols="4" class="d-flex justify-end me-5">
+      <div class="burger" @click="toggleMenu">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <div v-show="isOpen" class="dropdown-menu d-flex-lg ga-7">
+        <div class="dropdown-item">Accueil</div>
+        <div class="dropdown-item">À propos</div>
+        <div class="dropdown-item">Compétences</div>
+        <div class="dropdown-item">Portfolio</div>
+        <div class="dropdown-item">Contactez-moi</div>
+      </div>
     </v-col>
   </v-row>
 </template>
 
 <style scoped>
 .logo {
-  height: 6.25rem;
+  height: 5.25rem;
   border-radius: 999px;
+}
+
+.dropdown-menu {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.8);
+  z-index: 1000;
+  opacity: 0;
+}
+
+.dropdown-item {
+  padding: 10px;
+  cursor: pointer;
+  font-size: 3.5rem;
+  color: white;
+  font-weight: 700;
+  position: relative;
+  opacity: 0;
+}
+
+.dropdown-item::after {
+  content: "";
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  height: 3px;
+  width: 0%;
+  background-color: white;
+  transition: width 0.3s ease;
+}
+
+.dropdown-item:hover {
+  transform: scale(1.1);
+  color: #f0a500;
+}
+
+.dropdown-item:hover::after {
+  width: 100%;
+}
+
+.burger {
+  position: relative;
+  width: 60px;
+  height: 45px;
+  cursor: pointer;
+  z-index: 1001;
+  transition: ease 0.3s;
+}
+
+.burger:hover {
+  transform: scale(1.05);
+}
+
+.burger span {
+  display: block;
+  position: absolute;
+  height: 7px;
+  width: 50%;
+  background-color: black;
+  opacity: 1;
+  border-radius: 0 9px 9px 0;
+  transform: rotate(0deg);
+  transition: 0.25s ease-in-out;
+}
+
+.burger span:nth-child(odd) {
+  left: 0px;
+  border-radius: 9px 0 0 9px;
+}
+
+.burger span:nth-child(even) {
+  left: 50%;
+}
+
+.burger span:nth-child(1),
+.burger span:nth-child(2) {
+  top: 0px;
+}
+
+.burger span:nth-child(3),
+.burger span:nth-child(4) {
+  top: 18px;
+}
+
+.burger span:nth-child(5),
+.burger span:nth-child(6) {
+  top: 36px;
 }
 </style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,16 +1,39 @@
 <script setup>
-import HeaderComponent from '@/components/HeaderComponent.vue';
+import HeaderComponent from "@/components/HeaderComponent.vue";
 
 //
 </script>
 
 <template>
-  <HeaderComponent />
+  <HeaderComponent style="position: fixed; width: 100vw" />
 
-  <img src="../assets/codingMan.png" class="ms-15" style="border-radius: 999px; height: 400px;" alt="la">
+  <v-row class="ma-auto " style="height: 102vh">
+    <v-col class="d-flex left-col v-col-md-6 flex-column align-center justify-center">
+      <h2>Hello !</h2>
+      <p>Moi c'est</p>
+      <div class="names d-flex ga-5">
+        <div class="firstName" style="color: #17b9f9">Lionel</div>
+        <div class="lastName" style="color: #fe6e1f">Feuga</div>
+      </div>
+    </v-col>
+    <v-col class="d-flex align-center justify-center ">
+      <img
+        src="../assets/codingMan.png"
+        style="border-radius: 999px; height: 400px"
+        alt="la"
+      />
+    </v-col>
+  </v-row>
 
+  <div
+    class="bla"
+    style="width: 100vw; background-color: black; height: 150px"
+  ></div>
 </template>
 
 <style scoped>
-
+.left-col {
+  font-size: 5rem;
+  font-family: "Bebas Neue", serif;
+}
 </style>
